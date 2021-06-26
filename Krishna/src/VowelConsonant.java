8eaf5574b459017dcef23971493e23d9a0efbd6c
krishnamullapudi <mullapudiramakrishna66@gmail.com>
2021-06-26 14:42:59 +0530

    Code changed

--- Krishna/src/VowelConsonant.java
+++ Krishna/src/VowelConsonant.java
@@ -7,22 +7,37 @@ public class VowelConsonant {
         System.out.println("enter the alphabet");
         alphabet=sc.next().charAt(0);
         switch(alphabet){
-            case 'a' , 'A':
+            case 'a':
                 System.out.println("given alphabet is vowel");
                 break;
-            case 'e', 'E':
+            case 'e':
                 System.out.println("given alphabet is vowel");
                 break;
-            case 'i','I':
+            case 'i':
                 System.out.println("given alphabet is vowel");
                 break;
-            case 'o','O':
+            case 'o':
                 System.out.println("given alphabet is vowel");
                 break;
-            case 'u', 'U':
+            case 'u':
                 System.out.println("given alphabet is vowel");
                 break;
-            default:
+            case 'A':
+                System.out.println("given alphabet is vowel");
+                break;
+            case 'E':
+                System.out.println("given alphabet is vowel");
+                break;
+            case 'I':
+                System.out.println("given alphabet is vowel");
+                break;
+            case 'O':
+                System.out.println("given alphabet is vowel");
+                break;
+            case 'U':
+                System.out.println("given alphabet is vowel");
+                break;
+                default:
                 System.out.println("given alphabet is consonant");
                 break;
                 //sc.close();
