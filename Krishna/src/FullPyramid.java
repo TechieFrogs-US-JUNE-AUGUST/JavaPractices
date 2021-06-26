fcd901b6df591ff0f0fd9a46423b55ce67d12e39
krishnamullapudi <mullapudiramakrishna66@gmail.com>
2021-06-26 15:13:55 +0530

    final code

--- Krishna/src/FullPyramid.java
+++ Krishna/src/FullPyramid.java
@@ -1,19 +1,15 @@
-package Assignment.java;
 public class FullPyramid {
     public static void main(String[] args) {
-        int k=0;
-        for (int i=1;i<=5;++i)
-        {
-            for(int j=1;j<=5-i; ++j)
-            {
-                System.out.println(" ");
+        int k = 0;
+        for (int i = 1; i <= 5; ++i,k=0) {
+            for (int s = 1; s <= 5 - i; ++s) {
+                System.out.print(" ");
             }
-            while(k!=2*i-1)
-            {
-                System.out.println(" * ");
+            while (k != 2 * i - 1) {
+                System.out.print("* ");
                 ++k;
             }
-            System.out.println("out of loops");
+            System.out.println();
         }
     }
 }
