2b8775375389b0f66b87d6154a37f31b3f82624c
Tarak0477 <tarakramnunna@gmail.com>
2021-06-30 16:17:56 +0530

    Committing Classes

---
+++ Tarak/ParameterizedTriangle.java
@@ -0,0 +1,6 @@
+public class ParameterizedTriangle {
+    public static void main(String[] args) {
+        Triangle obj1=new Triangle(3,4,5);
+    }
+    
+}
---
+++ Tarak/TriangleAreaAndPerimeter.java
@@ -0,0 +1,25 @@
+class Triangle{
+int a=8;
+int b=6;
+int c=10;
+Triangle(){
+    int peri=a+b+c;
+    int Area=(a*b)/2;
+    System.out.println("perimeter of triangle in units "+peri);
+    System.out.println("Area of triangle in SQ units "+Area);
+}
+Triangle(int a,int b,int c){
+int peri=a+b+c;
+int Area=(a*b)/2;
+System.out.println("Perimeter of triangle in units "+peri);
+System.out.println("Area of Triangle in SQ units "+Area);
+}
+}
+
+
+public class TriangleAreaAndPerimeter {
+    public static void main(String[] args) {
+        Triangle obj = new Triangle();
+    }
+    
+}
