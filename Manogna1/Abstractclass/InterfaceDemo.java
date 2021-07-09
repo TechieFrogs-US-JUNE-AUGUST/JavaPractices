fa515233d921ea414ef4a5bf31da72888a2222f8
Manogna0 <smanogna0@gmail.com>
2021-07-09 13:13:20 -0400

    Commit

---
+++ Manogna1/Abstractclass/InterfaceDemo.java
@@ -0,0 +1,18 @@
+package Abstractclass;
+
+import Interface.Interface9;
+
+public class InterfaceDemo{
+    protected class InterfaceDemo1 implements Interface9{
+
+     
+        public InterfaceDemo1() {       //Why we neeed to create a default constructor
+        }
+
+        public void i1() {
+           System.out.println("i1");
+        }
+           
+    }
+}
+
