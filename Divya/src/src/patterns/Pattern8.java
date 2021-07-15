773e913cb8ab69c5f948af8ca0fab5578d05cee8
Divyadimili <sridivya.knm95@gmail.com>
2021-07-15 18:57:29 +0530

    new commit

---
+++ Divya/src/src/patterns/Pattern8.java
@@ -0,0 +1,10 @@
+class Patern8{
+    public static void main(String[] args){
+        for(char i='F';i>='A';i--){
+            for( char j='F';j>=i;j--){
+            System.out.print(j) ;
+            }
+        System.out.println();
+        }
+    }
+}
---
+++ Divya/src/src/patterns/PatternPage1.java
@@ -0,0 +1,15 @@
+ class PatternPage1{
+     public static void main(String[] args){
+         int i,j,n=5;
+         for(i=1;i<=n;i++)
+         {
+             for(j=n;j>=1;j--){
+                 if(j!=i)
+                 System.out.print(j);
+                 else
+                 System.out.print("*");
+             }
+             System.out.println("");
+         }
+     }
+ }
