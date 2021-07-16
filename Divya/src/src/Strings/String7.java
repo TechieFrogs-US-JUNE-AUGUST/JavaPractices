4d11ac8f70306f85306dc81117a4cd1568df7dd3
nalabothu233 <45651669+nalabothu233@users.noreply.github.com>
2021-07-16 11:15:41 -0700

    Revert "testing"
    This reverts commit 50203d02fa7b07c8da3f1c527a6c66655d43230e.

--- Divya/src/src/Strings/String7.java
+++
@@ -1,9 +0,0 @@
-package Strings;
-
-public class String7 {
-    public static void main(String[] args){
-        String s1=new String("divya");
-        String s2=new String("kavya");
-        System.out.println(s1==s2);
-    }
-}
--- Tarak/src/Patterns/PatternsAssignment1.java
+++
@@ -1,7 +0,0 @@
-package Patterns;
-
-public class PatternsAssignment1 {
-    public static void main(String[] args) {
-        
-    }
-}
