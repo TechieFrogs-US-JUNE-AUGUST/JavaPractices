0bdda6b93543bfb91473b00b2df1316de83e5ced
SaiBachina <saisree.y@gmail.com>
2021-07-16 09:10:19 -0500

    commit

--- Sai/src/Astring23.java
+++ Sai/src/Astring23.java
@@ -1,7 +1,7 @@
 public class Astring23{
     public static void main(String[] args){
         int p = 0;
-        String str = "Checking for punctuations, in this S!ring";
+        String str = "Checking for punctuations, in this S!ring.";
         for (int i = 0; i<str.length(); i++){
             if(str.charAt(i)=='!' || str.charAt(i)==',' || str.charAt(i)=='.' || str.charAt(i)=='?')
                 p++; //checking for spectial charecters, then only p increases
