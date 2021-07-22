fd6e51707fc00df55b7477531d23c9100f04f8ed
Manogna0 <smanogna0@gmail.com>
2021-07-22 12:32:07 -0400

    Commit

--- Manogna1/strings/String38.java
+++ Manogna1/strings/String38.java
@@ -2,29 +2,41 @@ package strings;
 
 public class String38 {
     public static void main(String[] args) {
-        String str="Strings Assignment Strings ";
-        str=str.toLowerCase();
+        String str="Strings Assignment Strings Program Strings";
+        //str=str.toLowerCase();
+        //int c=0;
        
         String words[]=str.split(" ");//Splits the string into words using build in fnctn
-        System.out.println("Duplicate words in a given string are :");
+        
+        //System.out.println(words.toString());
         //System.out.println(str.length());
         //System.out.println(words.length);
         for(int i=0;i<words.length;i++)
         {
-           
+            System.out.println(words[i]);
+        }
+        System.out.println("Duplicate words in a given string are :");
+        for(int i=0;i<words.length;i++)
+        {
+           int c=0;
             for(int j=i+1;j<words.length;j++)
             {
-               if(words[i].equals(words[j]));
+               if(words[i].equals(words[j]))
             
                 {
                    
-                    System.out.println(words[j]);
+                   c=1;
                     
                 }
             }
+            if(c==1)
+        {
+            System.out.println(words[i]);
+        }
             
     
         }
+        
     
     }
     
