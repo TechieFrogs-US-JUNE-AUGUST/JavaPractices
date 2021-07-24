22a4c57db68a8b87361da1fecf93aa9e63033f1c
[Krishnamullapudi] <[mullapudiramakrishna66@gmail.com]>
2021-07-24 23:50:17 +0530

    arrays assignment

---
+++ Krishna/src/Arrays/Array4.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Array4 
+{
+    public static void main(String[] args) 
+    {
+       int array[]=new int[]{1,1,2,2,3,5};
+
+       System.out.println("Duplicate elements in an Array :");
+
+       for(int i=0;i<array.length;i++)
+       {
+            for(int j=i+1;j<array.length;j++)
+            {
+                if(array[i]==array[j])
+
+                System.out.println(array[i]); 
+            }      
+       }
+    }
+}
