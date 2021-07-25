ceb3a4692655d4e4f707529e10948e27e2a9a289
SaiBachina <saisree.y@gmail.com>
2021-07-24 20:52:51 -0500

    commit again

--- Sai/src/Array22Sub.java
+++ Sai/src/Array22Sub.java
@@ -2,20 +2,20 @@ public class Array22Sub{
    public static void main(String args[]){
        int matrixA[][] = {{3,2},{3,4}};
        System.out.println("Matrix A : ");
-       display(matrixA);
+       display(matrixA);// calling method for printing
        int matrixB[][] = {{1,1},{2,2}};
        System.out.println("Matrix B : ");
-       display(matrixB);
+       display(matrixB);// calling method for printing
        int rows = matrixA.length;
        int columns = matrixA[0].length;
-       int substract[][] = new int[rows][columns];
-       for(int row = 0; row < matrixA.length; row++){
+       int substract[][] = new int[rows][columns];//array for substract
+       for(int row = 0; row < matrixA.length; row++){ //loop for to do substraction
           for(int column = 0; column < 2 ; column++){
               substract[row][column] = matrixA[row][column] - matrixB[row][column];
           }
        }
           System.out.println("Subtraction of two matrices: ");
-          display(substract); 
+          display(substract); // calling method for printing
     }
           /*for(int row = 0; row < rows; row++){  
               for(int column = 0; column < columns; column++){  
@@ -23,7 +23,7 @@ public class Array22Sub{
               }  
               System.out.println();*/  
     
-          static void display(int displayArray[][]){
+          static void display(int displayArray[][]){// creating method  for display an array
             for(int row = 0; row < 2; row++){  
                 for(int column = 0; column < 2; column++){  
                    System.out.print(displayArray[row][column] + " ");  
